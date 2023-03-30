8f004f067a28225663b1d5acf9e24d7438f5149f
RagurivonRaguri <lincolnmuraguri93@gmail.com>
2023-03-30 15:11:38 +0300

    Done

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let lowerCaseWord = word.toLowerCase();
+return lowerCaseWord === lowerCaseWord.split('').reverse().join('')
 }
 
+
 /* 
-  Add your pseudocode here
+  Add your pseudocode 
+  Create funtion'isPalindrome(str)
+  make the string in lowercase;
+  compare the string when its reading forwards or backwords and return true if it is else false
 */
-
-/*
-  Add written explanation of your solution here
-*/
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  // console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
