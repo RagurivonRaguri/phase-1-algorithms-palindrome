036e697e764cf36d3f8e0cde44a9070bad4508d9
RagurivonRaguri <lincolnmuraguri93@gmail.com>
2023-03-30 15:32:48 +0300

    Done

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  // console.log("");
+  console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
